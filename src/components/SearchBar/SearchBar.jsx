b095730911f95fd68dd690c9cffe6aeb8ee4c486
Harini <harinisenthilkumar888@gmail.com>
2026-07-04 12:09:42 +0530

    Add state and event handling to SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+    const [subreddit, setSubreddit] = useState("");
+
+    const handleAdd = () => {
+        console.log(subreddit);
+    };
+
     return (
         <div className="search-bar">
             <input
                 type="text"
                 placeholder="Enter subreddit name..."
+                value={subreddit}
+                onChange={(e) => setSubreddit(e.target.value)}
             />
 
-            <button>Add</button>
+            <button onClick={handleAdd}>Add</button>
         </div>
     );
 }
